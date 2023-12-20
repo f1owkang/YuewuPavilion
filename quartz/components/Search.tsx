3d7a896e2e744be5dea9f788562f7d98bfe651ca
lxdebug <todrives@outlook.com>
2023-12-20 17:20:11 +0800

    feat: 汉化部分文字

--- quartz/components/Search.tsx
+++ quartz/components/Search.tsx
@@ -8,7 +8,7 @@ export default (() => {
     return (
       <div class={`search ${displayClass ?? ""}`}>
         <div id="search-icon">
-          <p>Search</p>
+          <p>搜索</p>
           <div></div>
           <svg
             tabIndex={0}
@@ -33,7 +33,7 @@ export default (() => {
               name="search"
               type="text"
               aria-label="Search for something"
-              placeholder="Search for something"
+              placeholder="搜索一些东西..."
             />
             <div id="results-container"></div>
           </div>
